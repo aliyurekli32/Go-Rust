3aa82ba74209ee592cfb4ceb37a4aa8474f11504
root <root@DESKTOP-AIO0D9C.>
2022-07-23 09:24:00 -0500

    add hello world rust and test

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
+
 }
+
+pub fn hello() -> String {
+    "Hello, world".to_string()
+}
+
+//write test code for main above with rust
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = hello();
+        assert_eq!(result, "Hello, world");
+    }
+}
+
+
