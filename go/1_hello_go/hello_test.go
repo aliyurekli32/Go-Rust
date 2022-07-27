5e8159f6571c899bb3a5d4366283c084876ce7d2
root <root@DESKTOP-AIO0D9C.>
2022-07-27 09:24:00 -0500

    add hello world go and test empty name

--- go/1_hello_go/hello_test.go
+++ go/1_hello_go/hello_test.go
@@ -3,9 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
 }
