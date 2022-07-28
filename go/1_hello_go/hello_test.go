b9dfd48a78a4c8ca5abf3bf8d7654ca94075670c
root <root@DESKTOP-AIO0D9C.>
2022-07-28 09:24:00 -0500

    refactor hello world go and test empty name

--- go/1_hello_go/hello_test.go
+++ go/1_hello_go/hello_test.go
@@ -6,17 +6,22 @@ func TestHello(t *testing.T) {
 	t.Run("say hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+
+		assertCorrectMessage(t, got, want)
 	})
 
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
