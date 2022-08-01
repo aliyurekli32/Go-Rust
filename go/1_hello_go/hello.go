7693d2428e76a10a6c99087564c699b0cc36342e
root <root@DESKTOP-AIO0D9C.>
2022-08-01 06:24:00 -0500

    add spanish hello world go and test

--- go/1_hello_go/hello.go
+++ go/1_hello_go/hello.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 const englishHelloPrefix string = "Hello, "
+const spanishHelloPrefix string = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
