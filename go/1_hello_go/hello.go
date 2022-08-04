1452740d45f473d62dc0a538e8b99c7e791d7792
aliyurekli32 <adamjesperblack@gmail.com>
2022-08-04 16:24:00 +0300

    refactor french hello world go and test

--- go/1_hello_go/hello.go
+++ go/1_hello_go/hello.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix string = "Hello, "
 const spanishHelloPrefix string = "Hola, "
+const frenchHelloPrefix string = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
@@ -13,6 +15,10 @@ func Hello(name string, language string) string {
 
 	if language == spanish {
 		return spanishHelloPrefix + name
+	}
+
+	if language == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
--- go/1_hello_go/hello_test.go
+++ go/1_hello_go/hello_test.go
@@ -23,6 +23,12 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
