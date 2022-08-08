99f6880d1f690ed7c53f230a0ab799ecf9d1e160
aliyurekli32 <adamjesperblack@gmail.com>
2022-08-08 08:20:00 -0500

    add: french and spanish lang for rust and test

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -2,15 +2,23 @@ fn main() {
     println!("Hello, world!");
 
 }
+const ENGLISH_HELLO_PREFIX: &str = "Hello, ";
+const SPANISH_HELLO_PREFIX: &str = "Hola, ";
+const FRENCH_HELLO_PREFIX: &str = "Bonjour, ";
+const SPANISH: &str = "spanish";
+const FRENCH: &str = "french";
 
-pub fn hello( name: String ) -> String {
+pub fn hello( name: String, language: String ) -> String {
     if name == "" {
         return "Hello, World".to_string();
     }
-    "Hello, ".to_string() + &name.to_string()
+    let prefix = match language.as_str() {
+        SPANISH =>  &SPANISH_HELLO_PREFIX,
+        FRENCH =>  &FRENCH_HELLO_PREFIX,
+        _ => ENGLISH_HELLO_PREFIX,
+    };
+    return prefix.to_string() + &name.to_string()
 }
-
-//write test code for main above with rust
 
 #[cfg(test)]
 mod tests {
@@ -18,15 +26,28 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = hello(String::from("Chris"));
+        let result = hello(String::from("Chris"),String::from(""));
         assert_eq!(result, "Hello, Chris");
     }
 
     #[test]
     fn it_works_default() {
-        let result = hello(String::from(""));
+        let result = hello(String::from(""),String::from(""));
         assert_eq!(result, "Hello, World");
     }
+
+    #[test]
+    fn it_works_spanish() {
+        let result = hello(String::from("Elodie"),String::from("spanish"));
+        assert_eq!(result, "Hola, Elodie");
+    }
+
+    #[test]
+    fn it_works_french() {
+        let result = hello(String::from("Elodie"),String::from("french"));
+        assert_eq!(result, "Bonjour, Elodie");
+    }
+
 }
 
 
